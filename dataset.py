1617a584f27fcb64491a23ae4f893b25df0eb120
Haowei Lou <z5258575@ad.unsw.edu.au>
2025-03-19 17:57:15 +1100

    SavE

--- dataset.py
+++ dataset.py
@@ -12,12 +12,11 @@ def load_wav_to_torch(filename):
 emo_dict = {"neutral":0,"angry":1,"happy":2,"sad":3,"surprise":4}
 lang_dict = {"ch":0,"en":1}
 class ESD(torch.utils.data.Dataset):
-    def __init__(self, csv_path,sample_rate=16000, n_mels=80):
+    def __init__(self, csv_path,sample_rate=16000, n_mels=80,sep=","):
         super(ESD, self).__init__()
-        self.data = pd.read_csv(csv_path)
-        self.data["file_path"] = self.data["file_path"].apply(lambda x: x.replace("C:/","D:/"))
-        # self.data["file_path"] = self.data["file_path"].apply(lambda x: x.replace("C:/","/share/scratch/haoweilou/"))
-        # print(self.data)
+        self.data = pd.read_csv(csv_path,sep=sep)
+        # self.data["file_path"] = self.data["file_path"].apply(lambda x: x.replace("C:/","D:/"))
+        self.data["file_path"] = self.data["file_path"].apply(lambda x: x.replace("C:/","/share/scratch/haoweilou/"))
         self.sample_rate = sample_rate
 
         self.mel_transform = transforms.MelSpectrogram(
@@ -50,13 +49,8 @@ class ESD(torch.utils.data.Dataset):
         if os.path.exists(spec_path):
             return torch.load(spec_path)
         else: 
-            # Load and normalize audio
             audio, sr = load_wav_to_torch(audio_path)
-
-            # audio_norm = audio / self.max_wav_value  # Normalize to [-1, 1]
             audio_norm = audio.unsqueeze(0)  # Add channel dim
-
-            # Compute spectrogram
             spec = self.mel_transform(audio_norm)
             spec = torch.squeeze(spec, 0)
 
